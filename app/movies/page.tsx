62d4bf3d917c7d827ab91f5ea11b0a08ab92128f
itx-syed <saan63238@gmail.com>
2026-08-28 23:23:04 +0500

    feat: add movies section

---
+++ app/movies/page.tsx
@@ -0,0 +1,2 @@
+import { SectionPage } from "@/components/section-page";
+export default function MoviesPage() { return <SectionPage kicker="Movies" title="Your media catalogue, your way." description="A polished browsing layer ready to connect to your own legal movie metadata and content sources." items={["Browse", "Genres", "Search", "Details"]} />; }
